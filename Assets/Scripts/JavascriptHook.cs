0eab376dc137239d83f1d2ec9a65b94b8378fdd1
David J McClelland <davidjmcclelland@gmail.com>
2022-02-04 09:25:40 -0500

    Messages sent from browser to Unity and Unity to browser with template included

--- Assets/Scripts/JavascriptHook.cs
+++ Assets/Scripts/JavascriptHook.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.InteropServices;
 
 public class JavascriptHook : MonoBehaviour
 {
+    [DllImport("__Internal")]
+    private static extern void HelloString(string str);
     [SerializeField] private SpriteRenderer circleSpriteRenderer;
 
     public void TintRed()
     {
         circleSpriteRenderer.color = Color.red;
+        HelloString("The circle is Red");
     }
 
     public void TintGreen()
     {
         circleSpriteRenderer.color = Color.green;
+        HelloString("The circle is Green");
     }
 
     private void Update()
